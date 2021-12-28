c50912062d5514ad92646da612dcb40eb54bf8a7
D45Hub <denis.thiessen@gmx.de>
2021-12-28 13:52:33 +0100

    Added TranslationProvider Test

---
+++ src/test/java/com/ctrlcutter/frontend/test/translation/TranslationProviderTest.java
@@ -0,0 +1,48 @@
+package com.ctrlcutter.frontend.test.translation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+import com.ctrlcutter.frontend.util.TranslationProvider;
+
+public class TranslationProviderTest {
+
+    private static final String EXAMPLE_TRANSLATION_KEY = "custom_macros_text_header";
+
+    @Test
+    public void testProvidedLocales() {
+
+        final TranslationProvider translationProvider = new TranslationProvider();
+
+        assertEquals(2, translationProvider.getProvidedLocales().size());
+    }
+
+    @Test
+    public void testEnglishTranslation() {
+
+        final TranslationProvider translationProvider = new TranslationProvider();
+
+        assertEquals("Take Control of your Macros", translationProvider.getTranslation(EXAMPLE_TRANSLATION_KEY, Locale.ENGLISH));
+    }
+
+    @Test
+    public void testGermanTranslation() {
+
+        final TranslationProvider translationProvider = new TranslationProvider();
+
+        assertEquals("Übernimm die Kontrolle für deine Makros", translationProvider.getTranslation(EXAMPLE_TRANSLATION_KEY, Locale.GERMAN));
+    }
+
+    @Test
+    public void testMultipleTranslations() {
+
+        final TranslationProvider translationProvider = new TranslationProvider();
+
+        assertEquals("Take Control of your Macros", translationProvider.getTranslation(EXAMPLE_TRANSLATION_KEY, Locale.ENGLISH));
+        assertEquals("Übernimm die Kontrolle für deine Makros", translationProvider.getTranslation(EXAMPLE_TRANSLATION_KEY, Locale.GERMAN));
+    }
+
+}
