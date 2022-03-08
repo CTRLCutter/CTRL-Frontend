d6e45d4d91e3b403f6553d84d508ae13b1a88bf8
D45Hub <denis.thiessen@gmx.de>
2022-03-08 22:55:08 +0100

    Modified Shortcut Selection View Styling

--- src/main/java/com/ctrlcutter/frontend/views/shortcutselectionview/ShortcutSelectionView.java
+++ src/main/java/com/ctrlcutter/frontend/views/shortcutselectionview/ShortcutSelectionView.java
@@ -5,15 +5,17 @@ import java.util.List;
 import com.ctrlcutter.frontend.util.ui.ViewRedirectionUtility;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Shortcut Selection")
 @Route(value = "shortcut_selection")
 @CssImport("./themes/ctrlcutter/shortcutSelectionView.css")
-public class ShortcutSelectionView extends HorizontalLayout {
+public class ShortcutSelectionView extends VerticalLayout {
 
     public ShortcutSelectionView() {
 
@@ -22,6 +24,10 @@ public class ShortcutSelectionView extends HorizontalLayout {
         List<ShortcutSelectionTypes> shortcutTypes = ShortcutSelectionTypes.getAllTypes();
         HorizontalLayout itemLayout = new HorizontalLayout();
         itemLayout.setId("itemLayout");
+        
+        H1 selectionViewHeader = new H1(getTranslation("shortcut_selection_header"));
+        selectionViewHeader.setId("selectionViewHeader");
+        selectionViewHeader.setWidthFull();
 
         for (ShortcutSelectionTypes shortcutType : shortcutTypes) {
 
@@ -38,7 +44,7 @@ public class ShortcutSelectionView extends HorizontalLayout {
             itemLayout.add(shortcutDiv);
         }
 
-        add(itemLayout);
+        add(selectionViewHeader, itemLayout);
     }
 
 }
