1891544379dec2a14e15f21b8aac044f1611a6fc
D45Hub <denis.thiessen@gmx.de>
2022-03-29 11:38:10 +0200

    Added preparation to login event.

--- src/main/java/com/ctrlcutter/frontend/views/loginview/LoginView.java
+++ src/main/java/com/ctrlcutter/frontend/views/loginview/LoginView.java
@@ -1,7 +1,9 @@
 package com.ctrlcutter.frontend.views.loginview;
 
+import com.ctrlcutter.frontend.dtos.LoginUserDTO;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,11 +16,19 @@ public class LoginView extends HorizontalLayout {
     public LoginView() {
         setId("loginView");
         setHeightFull();
-        
+
         LoginForm loginForm = new LoginForm();
+        loginForm.setForgotPasswordButtonVisible(false);
         loginForm.setId("loginForm");
-        
+        loginForm.addLoginListener(e -> {
+            LoginUserDTO user = new LoginUserDTO(e.getUsername(), e.getPassword());
+            loginUser(user);
+        });
+
         add(loginForm);
     }
 
+    private void loginUser(LoginUserDTO user) {
+        Notification.show(user.getEmail() + " Login");
+    }
 }
